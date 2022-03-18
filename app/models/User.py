b6ca9eade7775ad53ff8567c253f979698118756
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2022-03-17 22:30:02 -0300

    :recycle: Alteração no model User.

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,13 @@
 from ..database import db
 
+"""
+==========================================================================
+ ➠ Sweet Taste Backend (https://github.com/RodrigoSiliunas/sweet-taste-flask)
+ ➠ Section By: Rodrigo Siliunas (Rô: https://github.com/RodrigoSiliunas)
+ ➠ Related system: User
+==========================================================================
+"""
+
 class User(db.Model):
     name = db.StringField(max_length=60, required=True)
     email = db.StringField(required=True, unique=True)
